08b27f9a450f34ca8626410e55b236f69045ba3b
subharthi75 <subharthi75@gmail.com>
2024-02-04 10:34:16 +0530

    img upld fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Button } from './ui/button'
+import React from "react";
+import { Button } from "./ui/button";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -9,22 +9,24 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
-  navigationMenuTriggerStyle
-} from "@/components/ui/navigation-menu"
-import { Link } from 'react-router-dom'
-import {logo} from '../assets/logo.png'
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 function Navbar() {
   return (
-    <div className='flex flex-row justify-between items-center w-full pt-4 pb-4'>
-      <div><img src={logo} alt="" /></div>
-      <div className='flex flex-row gap-8'>
+    <div className="flex flex-row justify-between items-center w-full pt-4 pb-4">
+      <div>
+        <img src={logo} alt="" />
+      </div>
+      <div className="flex flex-row gap-8">
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
               <NavigationMenuTrigger>Adopt a Pet</NavigationMenuTrigger>
               <NavigationMenuContent className="min-w-80 p-4">
-                <ul className='[&>li]:pt-2 [&>li]:pb-2'>
+                <ul className="[&>li]:pt-2 [&>li]:pb-2">
                   <li>
                     <NavigationMenuLink>Adopt a dog</NavigationMenuLink>
                   </li>
@@ -40,7 +42,7 @@ function Navbar() {
             <NavigationMenuItem>
               <NavigationMenuTrigger>Know Your Pet</NavigationMenuTrigger>
               <NavigationMenuContent className="min-w-80 p-4">
-                <ul className='[&>li]:pt-2 [&>li]:pb-2'>
+                <ul className="[&>li]:pt-2 [&>li]:pb-2">
                   <li>
                     <NavigationMenuLink>Pet chatbot</NavigationMenuLink>
                   </li>
@@ -54,25 +56,29 @@ function Navbar() {
               </NavigationMenuContent>
             </NavigationMenuItem>
             <NavigationMenuItem>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Rehome a pet
-                </NavigationMenuLink>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Rehome a pet
+              </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  About Us
-                </NavigationMenuLink>             
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                About Us
+              </NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
-        <div >
-          <Button variant="secondary"><Link to='/login' >Login</Link></Button>
+        <div>
+          <Button variant="secondary">
+            <Link to="/login">Login</Link>
+          </Button>
           &nbsp;
-          <Button><Link to='/Signup' >Signup</Link></Button>
+          <Button>
+            <Link to="/Signup">Signup</Link>
+          </Button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
